c8068a912bf11fc3e5052f5fed7942ca5fb5eb97
manCREEPman <barbekyan@yandex.ru>
2020-11-19 02:50:28 +0300

    Изменения базы: 1) добавлена процедура проверки авторизации сантехника 2) добавлена процедура обновления статуса изменения заказа другим оператором 3) добавлена процедура обновления статусов заказа и привязанного к нему сантехника В серверной части реализованы: 1) Регистрация/вход сантехника путём рефакторинга существующей процедуры регистрации/входа оператора 2) Добавлена серверная процедура на создание/редактирование заказа 3) Добавлена серверная процедура на связывание сантехника и заказа

--- PlumbersServer.dfm
+++ PlumbersServer.dfm
@@ -1,7 +1,7 @@
 object PlumbersResource1: TPlumbersResource1
   OldCreateOrder = False
-  Height = 351
-  Width = 575
+  Height = 467
+  Width = 775
   object FDPlumDBConnection: TFDConnection
     Params.Strings = (
       'Database=B:\Code\Projects\Plumbers potugi\PLUMBERAGENCY.FDB'
@@ -9,6 +9,7 @@ object PlumbersResource1: TPlumbersResource1
       'Password=masterkey'
       'CharacterSet=win1251'
       'DriverID=FB')
+    Connected = True
     LoginPrompt = False
     Left = 48
     Top = 24
@@ -41,7 +42,7 @@ object PlumbersResource1: TPlumbersResource1
     Left = 328
     Top = 88
   end
-  object FDCheckAuth: TFDStoredProc
+  object FDCheckOperatorAuth: TFDStoredProc
     Connection = FDPlumDBConnection
     StoredProcName = 'OPERATOR_CHECK_AUTH'
     Left = 512
@@ -112,6 +113,253 @@ object PlumbersResource1: TPlumbersResource1
         Name = 'OUT_ID'
         DataType = ftInteger
         ParamType = ptOutput
+      end>
+  end
+  object FDAddOrder: TFDStoredProc
+    Connection = FDPlumDBConnection
+    StoredProcName = 'ORDERS_ADD'
+    Left = 512
+    Top = 208
+    ParamData = <
+      item
+        Position = 1
+        Name = 'IN_ID'
+        DataType = ftInteger
+        ParamType = ptInput
+      end
+      item
+        Position = 2
+        Name = 'IN_CREATION_DATE'
+        DataType = ftDate
+        ParamType = ptInput
+      end
+      item
+        Position = 3
+        Name = 'IN_BEGIN_DATE'
+        DataType = ftDate
+        ParamType = ptInput
+      end
+      item
+        Position = 4
+        Name = 'IN_READ_DATE'
+        DataType = ftDate
+        ParamType = ptInput
+      end
+      item
+        Position = 5
+        Name = 'IN_END_DATE'
+        DataType = ftDate
+        ParamType = ptInput
+      end
+      item
+        Position = 6
+        Name = 'IN_STATUS'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 15
+      end
+      item
+        Position = 7
+        Name = 'IN_PLUMBER_ID'
+        DataType = ftInteger
+        ParamType = ptInput
+      end
+      item
+        Position = 8
+        Name = 'IN_OPERATOR_ID'
+        DataType = ftInteger
+        ParamType = ptInput
+      end
+      item
+        Position = 9
+        Name = 'OUT_ORDER_ID'
+        DataType = ftInteger
+        ParamType = ptOutput
+      end>
+  end
+  object FDAddOrderInfo: TFDStoredProc
+    Connection = FDPlumDBConnection
+    StoredProcName = 'ORDER_INFO_ADD'
+    Left = 512
+    Top = 264
+    ParamData = <
+      item
+        Position = 1
+        Name = 'IN_ORDER_ID'
+        DataType = ftInteger
+        ParamType = ptInput
+      end
+      item
+        Position = 2
+        Name = 'IN_TYPE'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 15
+      end
+      item
+        Position = 3
+        Name = 'IN_DESCRIPTION'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 500
+      end
+      item
+        Position = 4
+        Name = 'IN_PRICE'
+        DataType = ftFMTBcd
+        Precision = 15
+        NumericScale = 2
+        ParamType = ptInput
+      end
+      item
+        Position = 5
+        Name = 'IN_PHONE'
+        DataType = ftLargeint
+        ParamType = ptInput
+      end
+      item
+        Position = 6
+        Name = 'IN_ADRESS'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 100
+      end
+      item
+        Position = 7
+        Name = 'OUT_ID'
+        DataType = ftInteger
+        ParamType = ptOutput
+      end>
+  end
+  object FDChangeOpEditStatus: TFDStoredProc
+    Connection = FDPlumDBConnection
+    StoredProcName = 'CHANGE_OPERATOR_EDIT_STATUS'
+    Left = 512
+    Top = 320
+    ParamData = <
+      item
+        Position = 1
+        Name = 'IN_ID'
+        DataType = ftInteger
+        ParamType = ptInput
+      end
+      item
+        Position = 2
+        Name = 'IN_STATUS'
+        DataType = ftBoolean
+        ParamType = ptInput
+      end>
+  end
+  object FDCheckPlumberAuth: TFDStoredProc
+    Connection = FDPlumDBConnection
+    StoredProcName = 'PLUMBER_CHECK_AUTH'
+    Left = 640
+    Top = 88
+    ParamData = <
+      item
+        Position = 1
+        Name = 'IN_ONLY_LOGIN'
+        DataType = ftBoolean
+        ParamType = ptInput
+      end
+      item
+        Position = 2
+        Name = 'IN_LOGIN'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 50
+      end
+      item
+        Position = 3
+        Name = 'IN_PASSWORD'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 100
+      end
+      item
+        Position = 4
+        Name = 'OUT_PLUM_ID'
+        DataType = ftInteger
+        ParamType = ptOutput
+      end
+      item
+        Position = 5
+        Name = 'OUT_EQUALITY'
+        DataType = ftBoolean
+        ParamType = ptOutput
+      end>
+  end
+  object FDRegPlumber: TFDStoredProc
+    Connection = FDPlumDBConnection
+    StoredProcName = 'PLUMBER_ADD'
+    Left = 640
+    Top = 144
+    ParamData = <
+      item
+        Position = 1
+        Name = 'IN_ID'
+        DataType = ftInteger
+        ParamType = ptInput
+      end
+      item
+        Position = 2
+        Name = 'IN_STATUS'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 20
+      end
+      item
+        Position = 3
+        Name = 'IN_NAME'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 100
+      end
+      item
+        Position = 4
+        Name = 'IN_LOGIN'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 50
+      end
+      item
+        Position = 5
+        Name = 'IN_PASSWORD'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 100
+      end
+      item
+        Position = 6
+        Name = 'OUT_ID'
+        DataType = ftInteger
+        ParamType = ptOutput
+      end>
+  end
+  object FDUpdatePlumAndOrderStatus: TFDStoredProc
+    Connection = FDPlumDBConnection
+    StoredProcName = 'UPDATE_PLUMBER_AND_ORDER_STATUS'
+    Left = 640
+    Top = 208
+    ParamData = <
+      item
+        Position = 1
+        Name = 'IN_PLUMBER_ID'
+        DataType = ftInteger
+        ParamType = ptInput
+      end
+      item
+        Position = 2
+        Name = 'IN_ORDER_ID'
+        DataType = ftInteger
+        ParamType = ptInput
+      end
+      item
+        Position = 3
+        Name = 'IN_STATUS'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 15
       end>
   end
 end
--- PlumbersServer.pas
+++ PlumbersServer.pas
@@ -21,28 +21,36 @@ type
     FDSelectOrders: TFDQuery;
     FDSelectOrdersInfo: TFDQuery;
     FDSelectPlumbers: TFDQuery;
-    FDCheckAuth: TFDStoredProc;
+    FDCheckOperatorAuth: TFDStoredProc;
     FDRegOP: TFDStoredProc;
+    FDAddOrder: TFDStoredProc;
+    FDAddOrderInfo: TFDStoredProc;
+    FDChangeOpEditStatus: TFDStoredProc;
+    FDCheckPlumberAuth: TFDStoredProc;
+    FDRegPlumber: TFDStoredProc;
+    FDUpdatePlumAndOrderStatus: TFDStoredProc;
   published
     procedure Get(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-    [ResourceSuffix('{item}')]
-    procedure GetItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
     procedure Post(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-    [ResourceSuffix('{item}')]
-    procedure PutItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-    [ResourceSuffix('{item}')]
-    procedure DeleteItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
 
     function GetDbData(FDQuery: TFDQuery): TJSONObject;
     function GetAllDBData(): TJSONObject;
 
     function FormJSONResponseMessage(msgtype: string; msg: string): TJSONObject;
 
-    function CheckOperatorAuthorization(ARequest: TEndpointRequest; var opID: integer): Boolean;
-    function AuthOperator(ARequest: TEndpointRequest): TJSONObject;
+    function CheckAccountAuthorization(ARequest: TEndpointRequest; var ID: integer): Boolean;
+    function AuthAccount(ARequest: TEndpointRequest): TJSONObject;
 
-    function CheckOperatorLoginExists(Login: string): Boolean;
-    function RegisterOperator(ARequest: TEndpointRequest): TJSONObject;
+    function CheckAccountLoginExists(Login: string; AuthMode: integer): Boolean;
+    function RegisterAccount(ARequest: TEndpointRequest): TJSONObject;
+
+    function SetDateFormatSettings(): TFormatSettings;
+    procedure SetOperatorEditStatus(CreatorOpID: integer; EditorOpID: integer);
+    function AddOrChangeOrder(ResponseObject : TJSONObject): integer;
+    function AddOrChangeOrderInfo(ResponseObject: TJSONObject; AddedOrderID: integer) : integer;
+    function AddOrChangeOrderAndOrderInfo(ARequest: TEndpointRequest): TJSONObject;
+
+    function AssignPlumberToOrder(ARequest: TEndpointRequest): TJSONObject;
   end;
 
 implementation
@@ -59,24 +67,38 @@ begin
 end;
 
 
-function TPlumbersResource1.CheckOperatorAuthorization(ARequest: TEndpointRequest; var opID: integer): Boolean;
-var LoginStr, PasswordStr, HashedPassword: string;
+function TPlumbersResource1.CheckAccountAuthorization(ARequest: TEndpointRequest; var ID: integer): Boolean;
+var LoginStr, PasswordStr, HashedPassword, AuthModeStr: string;
 truth: Boolean;
+StoredProc: TFDStoredProc;
+AuthMode: integer;
 begin
+  Result:= false;
   ARequest.Params.TryGetValue('login', LoginStr);
   ARequest.Params.TryGetValue('password', PasswordStr);
+  ARequest.Params.TryGetValue('mode', AuthModeStr);
+  AuthMode:= StrToInt(AuthModeStr);
   HashedPassword:= THashSHA2.GetHashString(PasswordStr);
-  FDCheckAuth.Prepare;
-  FDCheckAuth.ParamByName('in_only_login').Value:= false;
-  FDCheckAuth.ParamByName('in_login').Value:= LoginStr;
-  FDCheckAuth.ParamByName('in_password').Value:= HashedPassword;
-//  FDCheckAuth.Params[0].Value:= HashedPassword;
-//  FDCheckAuth.Params[1].Value:= LoginStr;
-  FDCheckAuth.ExecProc;
-//  opID:= FDCheckAuth.Params.Items[2].AsInteger;
-//  Result:= FDCheckAuth.Params.Items[3].AsBoolean;
-opID:= FDCheckAuth.Params.FindParam('out_op_id').AsInteger;
-Result:= FDCheckAuth.Params.FindParam('out_equality').AsBoolean;
+
+  case AuthMode of
+    1: StoredProc:= FDCheckOperatorAuth;
+    2: StoredProc:= FDCheckPlumberAuth;
+  end;
+
+  if AuthMode <> 0 then begin
+    StoredProc.Prepare;
+    StoredProc.ParamByName('in_only_login').Value:= false;
+    StoredProc.ParamByName('in_login').Value:= LoginStr;
+    StoredProc.ParamByName('in_password').Value:= HashedPassword;
+    StoredProc.ExecProc;
+
+    case AuthMode of
+      1: ID:= StoredProc.Params.FindParam('out_op_id').AsInteger;
+      2: ID:= StoredProc.Params.FindParam('out_plum_id').AsInteger;
+    end;
+
+    Result:= StoredProc.Params.FindParam('out_equality').AsBoolean;
+  end;
 end;
 
 function TPlumbersResource1.GetDbData(FDQuery: TFDQuery): TJSONObject;
@@ -121,50 +143,203 @@ begin
     Result:= JSONResponse;
 end;
 
-function TPlumbersResource1.AuthOperator(ARequest: TEndpointRequest): TJSONObject;
-var opID: integer;
+function TPlumbersResource1.AuthAccount(ARequest: TEndpointRequest): TJSONObject;
+var ID, AuthMode: integer;
+AuthModeStr: string;
 begin
-    if CheckOperatorAuthorization(ARequest, opID) then begin
-      Result:= GetAllDBData();
-      Result.AddPair('opID', TJSONNumber.Create(opID));
+    Result:= TJSONObject.Create;
+    if CheckAccountAuthorization(ARequest, ID) then begin
+      ARequest.Params.TryGetValue('mode', AuthModeStr);
+      AuthMode:= StrToInt(AuthModeStr);
+      case AuthMode of
+      1:
+        begin
+          Result:= GetAllDBData();
+        end;
+      2:
+        begin
+        // В случае надобности добавить что-то сюда
+        end;
+      end;
+
+      Result.AddPair('ID', TJSONNumber.Create(ID));
     end
     else Result:= FormJSONResponseMessage('error', 'Invalid login or password');
 end;
 
-function TPlumbersResource1.CheckOperatorLoginExists(Login: string): Boolean;
+function TPlumbersResource1.CheckAccountLoginExists(Login: string; AuthMode: integer): Boolean;
+var StoredProc: TFDStoredProc;
 begin
-  FDCheckAuth.Prepare;
-  FDCheckAuth.ParamByName('in_login').Value:= Login;
-  FDCheckAuth.ParamByName('in_only_login').Value:= true;
-  FDCheckAuth.ExecProc;
-  Result:= FDCheckAuth.Params.FindParam('out_equality').AsBoolean;
+  Result:= false;
+
+  case AuthMode of
+    1: StoredProc:= FDCheckOperatorAuth;
+    2: StoredProc:= FDCheckPlumberAuth;
+  end;
+
+  if AuthMode <> 0 then begin
+    StoredProc.Prepare;
+    StoredProc.ParamByName('in_login').Value:= Login;
+    StoredProc.ParamByName('in_only_login').Value:= true;
+    StoredProc.ExecProc;
+    Result:= StoredProc.Params.FindParam('out_equality').AsBoolean;
+  end;
 end;
 
-function TPlumbersResource1.RegisterOperator(ARequest: TEndpointRequest): TJSONObject;
+function TPlumbersResource1.RegisterAccount(ARequest: TEndpointRequest): TJSONObject;
 var
-OperatorData: TJSONObject;
-Name, Login, Password: string;
+AuthMode: integer;
+AccountData: TJSONObject;
+Name, Login, Password, AuthModeStr: string;
+StoredProc: TFDStoredProc;
 begin
-  OperatorData:= TJSONObject.Create;
-  if ARequest.Body.TryGetObject(OperatorData) then begin
-      Login:= OperatorData.GetValue('login').Value;
-      if not CheckOperatorLoginExists(Login) then begin
-        Name:= OperatorData.GetValue('name').Value;
+  ARequest.Headers.TryGetValue('mode', AuthModeStr);
+  AuthMode:= StrToInt(AuthModeStr);
+  AccountData:= TJSONObject.Create;
+  if ARequest.Body.TryGetObject(AccountData) then begin
+      Login:= AccountData.GetValue('login').Value;
+      if not CheckAccountLoginExists(Login, AuthMode) then begin
+        Name:= AccountData.GetValue('name').Value;
 
-        Password:= OperatorData.GetValue('password').Value;
+        Password:= AccountData.GetValue('password').Value;
         Password:= THashSHA2.GetHashString(Password);
 
-        FDRegOP.Prepare;
-        FDRegOP.ParamByName('in_id').Value:= 0;
-        FDRegOP.ParamByName('in_name').Value:= Name;
-        FDRegOP.ParamByName('in_login').Value:= Login;
-        FDRegOP.ParamByName('in_password').Value:= Password;
-        FDRegOP.ParamByName('in_edit_status').Value:= false;
-        FDRegOP.ExecProc;
+        case AuthMode of
+          1: StoredProc:= FDRegOP;
+          2: StoredProc:= FDRegPlumber;
+        end;
 
+        StoredProc.Prepare;
+        StoredProc.ParamByName('in_id').Value:= 0;
+        StoredProc.ParamByName('in_name').Value:= Name;
+        StoredProc.ParamByName('in_login').Value:= Login;
+        StoredProc.ParamByName('in_password').Value:= Password;
+
+        case AuthMode of
+          1: StoredProc.ParamByName('in_edit_status').Value:= false;
+          2: StoredProc.ParamByName('in_status').Value:= 'Не в сети';
+        end;
+
+        StoredProc.ExecProc;
         Result:= FormJSONResponseMessage('response', 'Registration made successfully');
       end
       else Result:= FormJSONResponseMessage('error', 'This login is already used');
+  end
+  else Result:= FormJSONResponseMessage('error', 'JSON expected');
+end;
+
+
+function TPlumbersResource1.SetDateFormatSettings(): TFormatSettings;
+begin
+  Result := TFormatSettings.Create;
+  Result.DateSeparator := '.';
+  Result.ShortDateFormat := 'dd.MM.yyyy';
+  Result.TimeSeparator := ':';
+  Result.ShortTimeFormat := 'hh:mm';
+  Result.LongTimeFormat := 'hh:mm:ss';
+end;
+
+procedure TPlumbersResource1.SetOperatorEditStatus(CreatorOpID: integer; EditorOpID: integer);
+begin
+  if CreatorOpID <> EditorOpID then begin
+    FDChangeOpEditStatus.Prepare;
+    FDChangeOpEditStatus.ParamByName('in_id').Value:= EditorOpID;
+    FDChangeOpEditStatus.ParamByName('in_status').Value:= True;
+    FDChangeOpEditStatus.ExecProc;
+  end;
+end;
+
+function TPlumbersResource1.AddOrChangeOrder(ResponseObject : TJSONObject): integer;
+var
+OrderData: TJSONObject;
+ChangeOrCreateOrderID, OperatorID: integer;
+TmpOrderID, TmpOpID: TJSONNumber;
+DateFormatSettings: TFormatSettings;
+begin
+  if ResponseObject.TryGetValue('orderid', TmpOrderID) then
+    ChangeOrCreateOrderID:= TmpOrderID.Value.ToInteger;
+  if ResponseObject.TryGetValue('operatorid', TmpOpID) then
+    OperatorID:= TmpOpID.Value.ToInteger;
+  DateFormatSettings:= SetDateFormatSettings;
+  Result:= 0;
+
+  if ResponseObject.TryGetValue('orderdata', OrderData) then begin
+    FDAddOrder.Prepare;
+    FDAddOrder.ParamByName('in_id').Value:= ChangeOrCreateOrderID;
+
+    if ChangeOrCreateOrderID = 0 then
+    begin
+      FDAddOrder.ParamByName('in_creation_date').Value:= StrToDateTime(OrderData.GetValue('creation_date').Value, DateFormatSettings);
+      FDAddOrder.ParamByName('in_operator_id').Value:= OrderData.GetValue('order_operator_id').Value.ToInteger;
+      FDAddOrder.ParamByName('in_status').Value:= OrderData.GetValue('order_status').Value;
+      FDAddOrder.ExecProc;
+      Result:= FDAddOrder.Params.FindParam('out_order_id').AsInteger;
+    end
+    else begin
+      SetOperatorEditStatus(OrderData.GetValue('order_operator_id').Value.ToInteger, OperatorID);
+      FDAddOrder.ParamByName('in_status').Value:= OrderData.GetValue('order_status').Value;
+      FDAddOrder.ParamByName('in_plumber_id').Value:= OrderData.GetValue('order_plumber_id').Value.ToInteger;
+      FDAddOrder.ExecProc;
+      Result:= ChangeOrCreateOrderID;
+    end;
+  end;
+end;
+
+function TPlumbersResource1.AddOrChangeOrderInfo(ResponseObject: TJSONObject; AddedOrderID: integer): integer;
+var
+OrderInfoData: TJSONObject;
+begin
+    Result:= 0;
+    if ResponseObject.TryGetValue('orderinfodata', OrderInfoData) and (AddedOrderID <> 0) then begin
+      FDAddOrderInfo.Prepare;
+      FDAddOrderInfo.ParamByName('in_order_id').Value:= AddedOrderID;
+      FDAddOrderInfo.ParamByName('in_type').Value:= OrderInfoData.GetValue('type').Value;
+      FDAddOrderInfo.ParamByName('in_description').Value:= OrderInfoData.GetValue('description').Value;
+      FDAddOrderInfo.ParamByName('in_price').Value:= OrderInfoData.GetValue('price').Value.ToDouble;
+      FDAddOrderInfo.ParamByName('in_phone').Value:= OrderInfoData.GetValue('phone').Value.ToInt64;
+      FDAddOrderInfo.ParamByName('in_address').Value:= OrderInfoData.GetValue('address').Value;
+      FDAddOrderInfo.ExecProc;
+
+      Result:= FDAddOrderInfo.Params.FindParam('out_id').AsInteger;
+    end;
+end;
+
+function TPlumbersResource1.AddOrChangeOrderAndOrderInfo(ARequest: TEndpointRequest): TJSONObject;
+var
+ResponseObject: TJSONObject;
+AddedOrderID, AddedOrderInfoID: integer;
+begin
+  ResponseObject:= TJSONObject.Create;
+
+  if ARequest.Body.TryGetObject(ResponseObject) then begin
+    AddedOrderID:= AddOrChangeOrder(ResponseObject);
+    AddedOrderInfoID:= AddOrChangeOrderInfo(ResponseObject, AddedOrderID);
+
+    if (AddedOrderID <> 0) and (AddedOrderInfoID <> 0) then
+      Result:= FormJSONResponseMessage('response', 'New Data added')
+    else
+      Result:= FormJSONResponseMessage('error', 'Something went wrong');
+  end;
+
+end;
+
+function TPlumbersResource1.AssignPlumberToOrder(ARequest: TEndpointRequest): TJSONObject;
+var
+ResponseObject: TJSONObject;
+OrderID, PlumberID: integer;
+Status: string;
+begin
+  ResponseObject:= TJSONObject.Create;
+  if ARequest.Body.TryGetObject(ResponseObject) then begin
+    FDUpdatePlumAndOrderStatus.Prepare;
+    if ResponseObject.TryGetValue('order_id', OrderID) then
+      FDUpdatePlumAndOrderStatus.ParamByName('in_order_id').Value:= OrderID;
+    if ResponseObject.TryGetValue('plumber_id', PlumberID) then
+      FDUpdatePlumAndOrderStatus.ParamByName('in_plumber_id').Value:= PlumberID;
+    if ResponseObject.TryGetValue('status', Status) then
+      FDUpdatePlumAndOrderStatus.ParamByName('in_status').Value:= Status;
+    FDUpdatePlumAndOrderStatus.ExecProc;
+    Result:= FormJSONResponseMessage('response', 'Status updated');
   end
   else Result:= FormJSONResponseMessage('error', 'JSON expected');
 end;
@@ -176,18 +351,9 @@ begin
   JSONResponse:= TJSONObject.Create;
   ARequest.Headers.TryGetValue('task', QueryTask);
 
-  if QueryTask = 'auth' then JSONResponse:= AuthOperator(ARequest);
+  if QueryTask = 'auth' then JSONResponse:= AuthAccount(ARequest);
 
   AResponse.Body.SetValue(JSONResponse, True);
-end;
-
-procedure TPlumbersResource1.GetItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-var
-  LItem: string;
-begin
-  LItem := ARequest.Params.Values['item'];
-  // Sample code
-  AResponse.Body.SetValue(TJSONString.Create('Plumbers ' + LItem), True)
 end;
 
 procedure TPlumbersResource1.Post(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
@@ -197,23 +363,11 @@ begin
   JSONResponse:= TJSONObject.Create;
   ARequest.Headers.TryGetValue('task', QueryTask);
 
-  if QueryTask = 'reg' then JSONResponse:= RegisterOperator(ARequest);
+  if QueryTask = 'reg' then JSONResponse:= RegisterAccount(ARequest);
+  if QueryTask = 'order' then  JSONResponse:= AddOrChangeOrderAndOrderInfo(ARequest);
+  if QueryTask = 'assign' then JSONResponse:= AssignPlumberToOrder(ARequest);
 
   AResponse.Body.SetValue(JSONResponse, True);
-end;
-
-procedure TPlumbersResource1.PutItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-var
-  LItem: string;
-begin
-  LItem := ARequest.Params.Values['item'];
-end;
-
-procedure TPlumbersResource1.DeleteItem(const AContext: TEndpointContext; const ARequest: TEndpointRequest; const AResponse: TEndpointResponse);
-var
-  LItem: string;
-begin
-  LItem := ARequest.Params.Values['item'];
 end;
 
 procedure Register;
